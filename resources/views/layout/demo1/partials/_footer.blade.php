7effaa1d503aad1ac23c9ec14589b1cdbbcc3880
ryanatpicchu <ryan@picchu.io>
2023-12-19 15:48:41 +0800

    ryan 示意如何更新成果網站至github

--- resources/views/layout/demo1/partials/_footer.blade.php
+++ resources/views/layout/demo1/partials/_footer.blade.php
@@ -5,7 +5,7 @@
 		<!--begin::Copyright-->
 		<div class="text-dark order-2 order-md-1">
 			<span class="text-muted fw-semibold me-1">{{ date('Y') }}&copy;</span>
-			<a href="/" class="text-gray-800 text-hover-primary">Plurality</a>
+			<a href="/" class="text-gray-800 text-hover-primary">Plurality 管理後台</a>
 		</div>
 		<!--end::Copyright-->
 	</div>
